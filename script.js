dbbe4b5e70da2b305442ccfd151ba786f79b4fdf
Bhavyam-Bhutani <bbhutani2001@gmail.com>
2024-07-11 02:38:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("generateButton").addEventListener("click", generatePassword);
+});
+
+function generatePassword() {
+    var length = 12;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    var password = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("passwordDisplay").textContent = password;
+}
